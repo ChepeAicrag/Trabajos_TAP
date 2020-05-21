56c34d2b33b406c80dc12ac6faf4fe00d3dda370
JoseAngel <19angelgarcia00@gmail.com>
2020-05-21 10:08:25 -0500

    Agregando Elemento Lista y Mi adaptador
    Se ha creado el XML de elemento lista y la clase MiAdaptador

--- Unidad 5/AsteroidesApp/app/src/main/java/com/example/asteroidesapp/Puntuaciones.java
+++ Unidad 5/AsteroidesApp/app/src/main/java/com/example/asteroidesapp/Puntuaciones.java
@@ -2,7 +2,10 @@ package com.example.asteroidesapp;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -12,8 +15,18 @@ public class Puntuaciones extends ListActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.puntuaciones);
-        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.elemento_lista,R.id.titulo,
+        /*
+        setListAdapter(new ArrayAdapter<String>(this,R.layout.elemento_lista,R.id.titulo,
                 MainActivity.almacen.listaPuntuaciones(10)));
+        */
+        setListAdapter(new MiAdaptador(this,MainActivity.almacen.listaPuntuaciones(10)));
     }
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Object o = getListAdapter().getItem(position);
+        Toast.makeText(this,"Seleccion: " + Integer.toString(position) + " - " + o.toString(),
+                Toast.LENGTH_LONG).show();
+    }
 }
