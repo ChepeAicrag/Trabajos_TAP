ce41e616fe45a8f139fbdf591f56a6bd6a42d177
JoseAngel <19angelgarcia00@gmail.com>
2020-05-27 10:01:57 -0500

    App_Animacion
    Una app para test de animaciones...

---
+++ Unidad 5/AnimacionTween/app/src/main/java/itoaxaca/edu/animaciontween/MainActivity.java
@@ -0,0 +1,21 @@
+package itoaxaca.edu.animaciontween;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        Animation animacion = AnimationUtils.loadAnimation(this,R.anim.animacion);
+        textView.startAnimation(animacion);
+    }
+}
