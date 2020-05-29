c85c5c9f989c42471b40a5b2702d2aca2b7150ca
JoseAngel <19angelgarcia00@gmail.com>
2020-05-29 10:04:33 -0500

    Agregando Hilos
    No compila todavía, le falta el método activarMisil()

--- Unidad 5/AsteroidesApp/app/src/main/java/com/example/asteroidesapp/VistaJuego.java
+++ Unidad 5/AsteroidesApp/app/src/main/java/com/example/asteroidesapp/VistaJuego.java
@@ -13,6 +13,8 @@ import android.graphics.drawable.shapes.PathShape;
 import android.graphics.drawable.shapes.RectShape;
 import android.preference.PreferenceManager;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import java.util.Vector;
 
@@ -20,13 +22,26 @@ public class VistaJuego extends View {
 
     private Vector<Grafico> asteroides; // Vector con los asteroides;
     private int numAsteroides = 5,
-                numFragmentos = 3,
-                giroNave; // Incremento de direccion de la nave
+            numFragmentos = 3,
+            giroNave; // Incremento de direccion de la nave
     private double aceleracionNave; // Incremento de velocidad de la nave
     private Grafico nave;
     private static final int MAX_VELOCIDAD_NAVE = 20, // Velocidad maxima de la nave
-                             PASO_GIRO_NAVE = 5;
+                             PASO_GIRO_NAVE = 5,
+                             PERIODO_PROCESO = 50, // Para mover asteroide
+                             PASO_VELOCIDAD_MISIL = 12;
     private static final float PASO_ACELERACION_NAVE = 0.5f;
+    // HILOS PARA MOVER LOS ASTEROIDES
+    private ThreadJuego thread = new ThreadJuego();
+    private long ultimoProceso = 0;
+    // PARA MOVER LA NAVE CON TOUCH
+    private float mX = 0, mY = 0;
+    private boolean disparo = false;
+    // MISIL
+    private Grafico misil;
+    private boolean misilActivo = false;
+    private int tiempoMisil;
+
 
     public VistaJuego(Context context, AttributeSet attrs){
         super(context,attrs);
@@ -118,14 +133,126 @@ public class VistaJuego extends View {
                 asteroide.setCenY((int) (Math.random() * alto));
             }while (asteroide.distancia(nave) < (ancho + alto) / 5);
         }
+        ultimoProceso = System.currentTimeMillis();
+        thread.start();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        synchronized (asteroides){
         for ( Grafico asteroide : asteroides){
-            asteroide.dibujarGrafico(canvas);
+                asteroide.dibujarGrafico(canvas);
+            }
         }
         nave.dibujarGrafico(canvas);
     }
+
+    @Override
+    public boolean onKeyDown(int codigoTecla, KeyEvent evento) {
+        super.onKeyDown(codigoTecla,evento);
+        boolean procesada = false;
+        switch (codigoTecla){
+            case KeyEvent.KEYCODE_DPAD_UP:
+                aceleracionNave = +PASO_ACELERACION_NAVE;
+                break;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                giroNave = -PASO_GIRO_NAVE;
+                break;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                giroNave = +PASO_GIRO_NAVE;
+                break;
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+            case KeyEvent.KEYCODE_ENTER:
+            default:
+                procesada = false;
+                break;
+        }
+        return procesada;
+    }
+
+    @Override
+    public boolean onKeyUp(int codigoTecla, KeyEvent evento) {
+        super.onKeyDown(codigoTecla,evento);
+        boolean procesada = false;
+        switch (codigoTecla){
+            case KeyEvent.KEYCODE_DPAD_UP:
+                aceleracionNave = 0;
+                break;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                giroNave = 0;
+                break;
+            default:
+                procesada = false;
+                break;
+        }
+        return procesada;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        super.onTouchEvent(event);
+        float x = event.getX(), y = event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                disparo = true;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dx = Math.abs(x - mX),
+                      dy = Math.abs(y - mY);
+                if (dy < 6 && dx > 6) {
+                    giroNave = Math.round(x - mX) / 2;
+                    disparo = false;
+                }else if (dx < 6 && dy > 6){
+                    aceleracionNave = Math.round(mY - y) / 25;
+                    disparo = false;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                giroNave = 0;
+                aceleracionNave = 0;
+                if (disparo)
+                    activaMisil();
+                break;
+        }
+        mX = x;
+        mY = y;
+        return true;
+    }
+
+    public void actualizaFisica(){
+        long ahora = System.currentTimeMillis();
+        if (ultimoProceso + PERIODO_PROCESO > ahora){
+            return;// Salir si el periodo no se ha compulido
+        }
+
+        // Para ejecucion en tiempo real enecesitamos el retardo
+        double retardo = (ahora - ultimoProceso) / PERIODO_PROCESO;
+        ultimoProceso = ahora;
+
+        // Actualizar velocidaad y direccion de la nave
+        nave.setAngulo(int (nave.getAngulo() + giroNave + retardo));
+
+        double nIncX = nave.getIncX() + aceleracionNave * Math.cos(Math.toRadians(nave.getAngulo())),
+               nIncY = nave.getIncY() + aceleracionNave * Math.sin(Math.toRadians(nave.getAngulo()));
+        if(Math.hypot(nIncX,nIncY) <= MAX_VELOCIDAD_NAVE){
+            nave.setIncX(nIncX);
+            nave.setIncY(nIncY);
+        }
+        nave.incrementarPos(retardo); // Actualizar la posición
+        for (Grafico asteroide : asteroides)
+            asteroide.incrementarPos(retardo);
+    }
+
+    class ThreadJuego extends Thread {
+
+        @Override
+        public void run() {
+            while (true){
+                actualizaFisica();
+            }
+        }
+    }
+
 }
